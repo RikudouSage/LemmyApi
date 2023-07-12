53512194cad23cef62e54f54df75a5ac159af6ae
Dominik Chrastecky <dominik@chrastecky.cz>
2023-07-12 11:34:47 +0200

    Add new error code from 0.18.2

--- src/Enum/ErrorCode.php
+++ src/Enum/ErrorCode.php
@@ -16,12 +16,13 @@ enum ErrorCode: string
     case NotModOrAdmin = 'not_a_mod_or_admin';
     case InvalidPassword = 'invalid_password';
     case CommunityBan = 'community_ban';
+    case IncorrectLogin = 'incorrect_login';
 
     public function toException(): LemmyApiException
     {
         return match ($this) {
             self::UserNotFound => new UserNotFoundException('The user could not be found, check the username.'),
-            self::IncorrectCredentials => new IncorrectPasswordException('The provided username or password is not valid.'),
+            self::IncorrectCredentials, self::IncorrectLogin => new IncorrectPasswordException('The provided username or password is not valid.'),
             self::NotAdmin => new InsufficientPermissionsException('You must be an admin to perform this action.'),
             self::NotModOrAdmin => new InsufficientPermissionsException('You must be an admin or moderator to perform this action.'),
             self::InvalidPassword => new IncorrectPasswordException('The provided password is not valid'),
