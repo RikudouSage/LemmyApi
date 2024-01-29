dbb8235e898571ec92a97ba35bfbe20e35e30f57
Dominik Chrastecky <dominik@chrastecky.cz>
2024-01-29 13:58:42 +0100

    Fix comment aggregates

--- src/Response/Aggregates/CommentAggregates.php
+++ src/Response/Aggregates/CommentAggregates.php
@@ -3,13 +3,13 @@
 namespace Rikudou\LemmyApi\Response\Aggregates;
 
 use DateTimeInterface;
+use JetBrains\PhpStorm\Deprecated;
 use Rikudou\LemmyApi\Attribute\Since;
 use Rikudou\LemmyApi\Response\AbstractResponseDto;
 
 final readonly class CommentAggregates extends AbstractResponseDto
 {
     public function __construct(
-        public int $id,
         public int $commentId,
         public int $score,
         public int $upvotes,
@@ -19,6 +19,8 @@ final readonly class CommentAggregates extends AbstractResponseDto
         public float $hotRank,
         #[Since('0.19.0')]
         public ?int $controversyRank = null,
+        #[Deprecated('0.19')]
+        public ?int $id = null,
     ) {
     }
 }
